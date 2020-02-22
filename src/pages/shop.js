934ecc14d4ffda3126ef68170d6f6612f730a691
Rich Andrews <richandrewsfl@gmail.com>
2020-02-21 20:43:56 -0500

    added snipcart.css and adjust shop with snipcart code

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import '../components/snipcart.css'
 
-const NotFoundPage = () => (
+const ShopPage = () => (
   <Layout>
     <div>
       <h1>Hello World Shop</h1>
       <p>Welcome to my future shop</p>
     </div>
+    <div id="snipcart" data-api-key="NmEyYTg0OWQtNDdjOC00OTkyLWFhYzYtMDJmZTZiNDJjNWI5NjM3MTIyMjgwMTY0MjEyOTkx" hidden></div>
+<script src="https://cdn.snipcart.com/themes/v3.0.8/default/snipcart.js"></script>
+
   </Layout>
 )
 
-export default NotFoundPage
+export default ShopPage
