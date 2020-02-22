c569f62696482f5104d874b8831c2258b7afd070
Rich Andrews <richandrewsfl@gmail.com>
2020-02-21 20:29:03 -0500

    Created ship.js

---
+++ src/pages/shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const NotFoundPage = () => (
+  <Layout>
+    <div>
+      <h1>Hello World Shop/h1>
+      <p>Welcome to my future shop</p>
+    </div>
+  </Layout>
+)
+
+export default NotFoundPage
