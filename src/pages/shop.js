002965b7e01791df270f8f6d272fbab4baf603c8
Rich Andrews <richandrewsfl@gmail.com>
2020-02-21 20:55:31 -0500

    Drone Photo and add to cart button

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -7,6 +7,20 @@ const ShopPage = () => (
     <div>
       <h1>Hello World Shop</h1>
       <p>Welcome to my future shop</p>
+    
+      <button class="snipcart-add-item"
+  data-item-id="drone"
+  data-item-price="1.99"
+  data-item-url="/shop"
+  data-item-description="One of my best drone photos"
+  data-item-image="../img/drone.jpg"
+  data-item-name="Drone"
+  data-item-custom1-name="Frame color"
+  data-item-custom1-options="Black|Brown|Gold">
+  Add to Cart
+</button>
+
+
     </div>
     <div id="snipcart" data-api-key="NmEyYTg0OWQtNDdjOC00OTkyLWFhYzYtMDJmZTZiNDJjNWI5NjM3MTIyMjgwMTY0MjEyOTkx" hidden></div>
 <script src="https://cdn.snipcart.com/themes/v3.0.8/default/snipcart.js"></script>
